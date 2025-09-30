8deb2a067632b74be4f938a0600f854784890ca4
Brayden Idzenga <braydenidzenga87@gmail.com>
2025-09-30 02:06:09 -0400

    changing to vanilla react because vite was being a huge pain in the ass

--- frontend/src/components/ui/provider.jsx
+++
@@ -1,7 +0,0 @@
-// src/components/ui/provider.jsx
-import { ChakraProvider } from '@chakra-ui/react';
-import theme from './theme';
-
-export const UIProvider = ({ children }) => {
-  return <ChakraProvider theme={theme}>{children}</ChakraProvider>;
-};

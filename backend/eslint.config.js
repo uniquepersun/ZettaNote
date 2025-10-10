4dc78221960cc06675ac242f13fddbdd73366f2e
Rabindra Nath Chanda <jitchanda7@gmail.com>
2025-10-10 10:06:14 +0530

    Improved & Added ESLint support for backend and only run checks or builds on chnages.

---
+++ backend/eslint.config.js
@@ -0,0 +1,27 @@
+import js from '@eslint/js';
+import globals from 'globals';
+
+export default [
+  { ignores: ['node_modules', 'dist'] },
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      sourceType: 'module',
+      globals: {
+        ...globals.node,
+      },
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+
+      'no-unused-vars': 'warn',
+      'no-undef': 'error',
+      'eqeqeq': ['error', 'always'],
+      'curly': 'error',
+      'prefer-const': 'warn',
+      'no-var': 'error',
+      'no-debugger': 'error',
+    },
+  },
+];

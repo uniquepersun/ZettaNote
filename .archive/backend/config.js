81085f26d3cd6cf80abcb0692bd8adb6ab4d7e2d
MannuVilasara <mannuvilasara@gmail.com>
2025-10-12 19:16:55 +0530

    rm -rf .archive ♻

--- .archive/backend/config.js
+++
@@ -1,6 +0,0 @@
-import dotenv from 'dotenv';
-dotenv.config();
-
-export const DB = process.env.DB;
-export const PORT = process.env.PORT;
-export const JWT_SECRET = process.env.JWT_SECRET;
--- .archive/frontend/src/components/ui/provider.jsx
+++
@@ -1,6 +0,0 @@
-import * as React from 'react';
-import { ThemeContextProvider } from './ThemeContext';
-
-export const UIProvider = ({ children }) => {
-  return <ThemeContextProvider>{children}</ThemeContextProvider>;
-};
